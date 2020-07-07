88d0275a1739a959c04b2bfd6c94faa44eb960c0
Matheus Cabral <48923315+MatheusCbrl@users.noreply.github.com>
2020-07-07 09:46:04 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,29 @@
+
+import mongoose from 'mongoose';
+import express from 'express';
+import routes from './routes/index.routes.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+//const uri = process.env.DB_URI;
+const uri = process.env.MONGODB_URI;
+
+const runApp = async () => {
+  const app = express();
+  app.use(express.json());
+
+  app.use(routes);
+
+  await mongoose.connect(uri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('servidor iniciado na portal 3000.');
+  })
+};
+
+runApp();
